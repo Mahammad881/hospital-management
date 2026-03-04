a7a978e87a4ace96a8abaf1748dbf3b37048d811
Mahammad881 <mh5676023@gmail.com>
2026-03-05 03:40:41 +0530

    db changes entire

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,5 @@
 const mysql = require("mysql2");
 
-console.log("DB_HOST:", process.env.DB_HOST);
-
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = db.promise();
