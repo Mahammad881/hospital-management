912f3aaa081271e827e5d85116fa715ab71dbabd
Mahammad881 <mh5676023@gmail.com>
2026-03-09 22:19:04 +0530

    changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,8 @@ const db = require("../config/db");
 
 const router = express.Router();
 
-// Register
+
+// REGISTER
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -14,26 +15,30 @@ router.post("/register", async (req, res) => {
 
     await db.query(
       "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
-      [name, email, hashedPassword],
+      [name, email, hashedPassword]
     );
 
     res.json({ message: "User registered successfully" });
+
   } catch (error) {
     console.error("REGISTER ERROR:", error);
     res.status(500).json({ error: error.message });
   }
 });
 
-// Login
+
+// LOGIN
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+    const result = await db.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
 
     const rows = result.rows;
+
     if (rows.length === 0) {
       return res.status(400).json({ message: "User not found" });
     }
@@ -41,6 +46,7 @@ router.post("/login", async (req, res) => {
     const user = rows[0];
 
     const validPass = await bcrypt.compare(password, user.password);
+
     if (!validPass) {
       return res.status(400).json({ message: "Invalid password" });
     }
@@ -48,14 +54,15 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" },
+      { expiresIn: "1d" }
     );
 
     res.json({
       message: "Login successful",
       token,
-      role: user.role,
+      role: user.role
     });
+
   } catch (error) {
     console.error("LOGIN ERROR:", error);
     res.status(500).json({ error: error.message });
